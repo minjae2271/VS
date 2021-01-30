eb1ced9a4048450051fc33e4b726852b15b7854e
MinJaeGwon <minjae2271@naver.com>
2021-01-30 14:38:09 +0900

    toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <v-app>
+    <div>
+      <v-toolbar>
+        <v-toolbar-title>
+          <nuxt-link to="/">sex</nuxt-link>
+        </v-toolbar-title>
+      </v-toolbar>
+    </div>
+  </v-app>
 </template>
 
 <script>
