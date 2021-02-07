d879b06de8f50dece70e9871a54b64bc27e9eff2
MinJaeGwon <minjae2271@naver.com>
2021-02-07 17:07:29 +0900

    ?

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,91 +1,100 @@
 <template>
   <v-app>
-    <v-main>
-      <div>
-        <v-app-bar dark color="green" fixed>
-          <v-toolbar-title>
-            <nuxt-link to="/">Versus</nuxt-link>
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <!-- <v-text-field
+    <div>
+      <v-app-bar dark color="green" fixed>
+        <v-toolbar-title>
+          <nuxt-link to="/">Versus</nuxt-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <!-- <v-text-field
             hide-details
             label="search"
             prepend-icon="mdi-magnify"
             :style="{ display: 'flex', alignItems: 'center' }"
           /> -->
-            <v-container
-              v-if="!me"
-              :style="{ display: 'flex', alignItems: 'center', width: '200px' }"
+          <v-container
+            v-if="!me"
+            :style="{ display: 'flex', alignItems: 'center', width: '200px' }"
+          >
+            <v-btn text @click="showLoginDialog">
+              <div>로그인</div>
+            </v-btn>
+            <v-btn
+              text
+              nuxt
+              to="/signup"
+              :style="{ display: 'flex', alignItems: 'center' }"
             >
-              <v-btn text @click="showLoginDialog">
-                <div>로그인</div>
-              </v-btn>
-              <v-btn
+              회원가입
+            </v-btn>
+          </v-container>
+          <v-container
+            v-else
+            :style="{ display: 'flex', alignItems: 'center', width: '200px' }"
+          >
+            <v-menu
+            v-model="profileMenu"
+            offset-y
+            :rounded="rounded">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              x-large
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-avatar
+              color="blue"
+              size="48">
+              <span class="white--text headline">{{me.nickname}}</span>
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item class="profile-menu-avatar">
+              <v-avatar
+              color="blue">
+                <span class="white--text headline">{{me.nickname}}</span>
+              </v-avatar>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                {{me.email}}
+              </v-list-item-title>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list-item>
+              <v-list-item-content>
+                <v-btn
                 text
                 nuxt
-                to="/signup"
+                to="/profile"
                 :style="{ display: 'flex', alignItems: 'center' }"
-              >
-                회원가입
+                >
+                <div>프로필</div>
               </v-btn>
-            </v-container>
-            <v-container
-              v-else
-              :style="{ display: 'flex', alignItems: 'center', width: '200px' }"
-            >
-              <v-menu v-model="profileMenu" offset-y :rounded="rounded">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon x-large v-bind="attrs" v-on="on">
-                    <v-avatar color="brown" size="48">
-                      <span class="white--text headline">{{
-                        me.nickname
-                      }}</span>
-                    </v-avatar>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item>
-                    <v-list-item-title>
-                      {{ me.email }}
-                    </v-list-item-title>
-                  </v-list-item>
-                  <v-divider></v-divider>
-                  <v-list-item>
-                    <v-list-item-content>
-                      <v-btn
-                        text
-                        nuxt
-                        to="/profile"
-                        :style="{ display: 'flex', alignItems: 'center' }"
-                      >
-                        <div>프로필</div>
-                      </v-btn>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-content>
-                      <v-btn text @click="logOut">
-                        <div>로그아웃</div>
-                      </v-btn>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-container>
-          </v-toolbar-items>
-        </v-app-bar>
-        <v-row no-gutters justify="center">
-          <v-col>
-            <nuxt />
-          </v-col>
-        </v-row>
-      </div>
-      <login-form
-        :dialogprop="showDialog"
-        v-on:dialogChange="showLoginDialog"
-      />
-    </v-main>
+              </v-list-item-content>
+            </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-btn text @click="logOut">
+                <div>로그아웃</div>
+              </v-btn>
+            </v-list-item-content>
+          </v-list-item>
+          </v-list>
+        </v-menu>
+          </v-container>
+        </v-toolbar-items>
+      </v-app-bar>
+      <v-row no-gutters justify="center">
+        <v-col>
+          <nuxt />
+        </v-col>
+      </v-row>
+    </div>
+    <login-form :dialogprop="showDialog" v-on:dialogChange="showLoginDialog" />
     <versus-footer />
   </v-app>
 </template>
@@ -133,7 +142,8 @@ a {
   display: block;
 }
 
-.profile-menu-avatar {
+.profile-menu-avatar
+{
   justify-content: center;
 }
 </style>
