f6a18d46cadb70a20801a4671492ae87ac64a66e
Ohhyeun Kwon <kdhgus92@gmail.com>
2021-02-28 00:12:39 +0900

    oh

--- front/components/PostForm.vue
+++ front/components/PostForm.vue
@@ -1,24 +1,22 @@
 <template>
   <v-container d-flex>
-      <v-card>
-          <v-subheader>글작성</v-subheader>
-      </v-card>
-      <v-card>
-          gd
-      </v-card>
+    <v-card>
+      <v-subheader>글작성</v-subheader>
+    </v-card>
+    <v-card>
+      gd
+    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            vaild: false
-        }
-    }
-}
+  data() {
+    return {
+      vaild: false
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
