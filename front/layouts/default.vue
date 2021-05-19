f351392d1a48f224f6b51674ae611ea18261be5a
Ohhyeun Kwon <kdhgus92@gmail.com>
2021-05-19 16:36:25 +0900

    210519 oh desktop

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,28 +1,5 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-app-bar app>
       <v-app-bar-title>
         <nuxt-link to="/">Murpik</nuxt-link>
